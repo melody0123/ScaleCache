1ccd10756aa615d32c18c5edbee3b47d37a1f48b
melody0123 <88380000+melody0123@users.noreply.github.com>
2025-08-15 15:11:43 -0600

    add libaudit-dev to the dependency list

--- kernel/tools/perf/install-deps.sh
+++ kernel/tools/perf/install-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install libdw-dev systemtap-sdt-dev libunwind-dev libslang2-dev libgtk2.0-dev libperl-dev python-dev binutils-dev libiberty-dev liblzma-dev libzstd-dev libcap-dev libnuma-dev libbabeltrace-dev libbabeltrace-ctf-dev
+sudo apt install libdw-dev systemtap-sdt-dev libunwind-dev libslang2-dev libgtk2.0-dev libperl-dev python-dev binutils-dev libiberty-dev liblzma-dev libzstd-dev libcap-dev libnuma-dev libbabeltrace-dev libbabeltrace-ctf-dev libaudit-dev
